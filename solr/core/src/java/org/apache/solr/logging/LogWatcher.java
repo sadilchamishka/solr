31e338839cafdcdba338d654a2d0af28debd9944
sadilchamishka <sandilchamishka@gmail.com>
2022-03-04 10:39:24 +0530

    Removed log4j1 usages:

--- solr/core/src/java/org/apache/solr/logging/LogWatcher.java
+++ solr/core/src/java/org/apache/solr/logging/LogWatcher.java
@@ -20,7 +20,7 @@ import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.core.SolrResourceLoader;
 import org.apache.solr.logging.jul.JulWatcher;
-import org.apache.solr.logging.log4j.Log4jWatcher;
+import org.apache.solr.logging.log4j2.Log4j2Watcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.impl.StaticLoggerBinder;
@@ -33,36 +33,36 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * A Class to monitor Logging events and hold N events in memory
- * 
+ *
  * This is abstract so we can support both JUL and Log4j (and other logging platforms)
  */
 public abstract class LogWatcher<E> {
 
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-  
+
   protected CircularList<E> history;
   protected long last = -1;
-  
+
   /**
    * @return The implementation name
    */
   public abstract String getName();
-  
+
   /**
    * @return The valid level names for this framework
    */
   public abstract List<String> getAllLevels();
-  
+
   /**
    * Sets the log level within this framework
    */
   public abstract void setLogLevel(String category, String level);
-  
+
   /**
    * @return all registered loggers
    */
   public abstract Collection<LoggerInfo> getAllLoggers();
-  
+
   public abstract void setThreshold(String level);
   public abstract String getThreshold();
 
@@ -70,20 +70,20 @@ public abstract class LogWatcher<E> {
     history.add(event);
     last = timstamp;
   }
-  
+
   public long getLastEvent() {
     return last;
   }
-  
+
   public int getHistorySize() {
     return (history==null) ? -1 : history.getBufferSize();
   }
-  
+
   public SolrDocumentList getHistory(long since, AtomicBoolean found) {
     if(history==null) {
       return null;
     }
-    
+
     SolrDocumentList docs = new SolrDocumentList();
     Iterator<E> iter = history.iterator();
     while(iter.hasNext()) {
@@ -101,10 +101,10 @@ public abstract class LogWatcher<E> {
     docs.setNumFound(docs.size()); // make it not look too funny
     return docs;
   }
-  
+
   public abstract long getTimestamp(E event);
   public abstract SolrDocument toSolrDocument(E event);
-  
+
   public abstract void registerListener(ListenerConfig cfg);
 
   public void reset() {
@@ -124,18 +124,20 @@ public abstract class LogWatcher<E> {
    *
    * @return a LogWatcher configured for the container's logging framework
    */
-  public static LogWatcher newRegisteredLogWatcher(LogWatcherConfig config, SolrResourceLoader loader) {
+  public static LogWatcher<?> newRegisteredLogWatcher(LogWatcherConfig config, SolrResourceLoader loader) {
 
     if (!config.isEnabled()) {
-      log.info("A LogWatcher is not enabled");
+      log.debug("A LogWatcher is not enabled");
       return null;
     }
 
-    LogWatcher logWatcher = createWatcher(config, loader);
+    LogWatcher<?> logWatcher = createWatcher(config, loader);
 
     if (logWatcher != null) {
       if (config.getWatcherSize() > 0) {
-        log.info("Registering Log Listener [{}]", logWatcher.getName());
+        if (log.isDebugEnabled()) {
+          log.debug("Registering Log Listener [{}]", logWatcher.getName());
+        }
         logWatcher.registerListener(config.asListenerConfig());
       }
     }
@@ -143,24 +145,24 @@ public abstract class LogWatcher<E> {
     return logWatcher;
   }
 
-  private static LogWatcher createWatcher(LogWatcherConfig config, SolrResourceLoader loader) {
+  private static LogWatcher<?> createWatcher(LogWatcherConfig config, SolrResourceLoader loader) {
 
     String fname = config.getLoggingClass();
     String slf4jImpl;
 
     try {
       slf4jImpl = StaticLoggerBinder.getSingleton().getLoggerFactoryClassStr();
-      log.info("SLF4J impl is " + slf4jImpl);
+      log.debug("SLF4J impl is {}", slf4jImpl);
       if (fname == null) {
-        if ("org.slf4j.impl.Log4jLoggerFactory".equals(slf4jImpl)) {
-          fname = "Log4j";
+        if ("org.apache.logging.slf4j.Log4jLoggerFactory".equals(slf4jImpl)) {
+          fname = "Log4j2";
         } else if (slf4jImpl.indexOf("JDK") > 0) {
           fname = "JUL";
         }
       }
     }
     catch (Throwable e) {
-      log.warn("Unable to read SLF4J version.  LogWatcher will be disabled: " + e);
+      log.warn("Unable to read SLF4J version.  LogWatcher will be disabled: ", e);
       if (e instanceof OutOfMemoryError) {
         throw (OutOfMemoryError) e;
       }
@@ -168,14 +170,16 @@ public abstract class LogWatcher<E> {
     }
 
     if (fname == null) {
-      log.info("No LogWatcher configured");
+      log.debug("No LogWatcher configured");
       return null;
     }
 
-    if ("JUL".equalsIgnoreCase(fname))
+    if ("JUL".equalsIgnoreCase(fname)) {
       return new JulWatcher(slf4jImpl);
-    if ("Log4j".equals(fname))
-      return new Log4jWatcher(slf4jImpl);
+    }
+    if ("Log4j2".equals(fname)) {
+      return new Log4j2Watcher();
+    }
 
     try {
       return loader != null ? loader.newInstance(fname, LogWatcher.class) : null;
